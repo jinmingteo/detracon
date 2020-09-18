c270671e8f9c894b5c1f7a1af2699abd75892428
jinster <jinmingteo@gmail.com>
2020-09-18 18:07:17 +0800

    fixed bugs

--- run_jetson.py
+++ run_jetson.py
@@ -21,26 +21,31 @@
 # DEALINGS IN THE SOFTWARE.
 #
 
+import cv2
 import jetson.inference
 import jetson.utils
 
 import argparse
 import sys
 
-import cv2
+
 from deep_sort_realtime.deepsort_tracker_emb import DeepSort as Tracker
 from utils.drawer import Drawer
 
+f = open('coco.txt','r')
+labels = f.read()
+label_arr = labels.split('\n')
+
 # parse the command line
-parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
-                                 formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +
-                                 jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.",
+								 formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +
+																					   jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
 
 parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
 parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
 parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
 parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
-parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use")
 
 is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
 
@@ -59,46 +64,50 @@ input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
 output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
 
 # assuming 7fps & 70nn_budget, tracker looks into 10secs in the past.
+nn_budget = 70
 tracker = Tracker(
-    max_age=30, nn_budget=nn_budget, override_track_class=None, clock=clock
-)
+	max_age=30, nn_budget=nn_budget, override_track_class=None)
 drawer = Drawer()
 # process frames until the user exits
 while True:
 	# capture the next image
 	img = input.Capture()
- 
-    # https://forums.developer.nvidia.com/t/videosource-capture-s-cudaimage-to-numpy/141093/5
-    np_source = jetson.utils.cudaToNumpy(img)
-    np_source = cv2.cvtColor(np_source, cv2.COLOR_RGBA2BGR)
+	np_source = jetson.utils.cudaToNumpy(img)
+	np_source = cv2.cvtColor(np_source, cv2.COLOR_RGBA2BGR)
 	# detect objects in the image (with overlay)
 	#detections = net.Detect(img, overlay=opt.overlay)
-    detections = net.Detect(img, overlay='none')
-    chosen_track = None
-    
+	detections = net.Detect(img, overlay='none')
+	chosen_track = None
+
 	# print the detections
 	print("detected {:d} objects in image".format(len(detections)))
-    raw_dets = []
+	raw_dets = []
 	for detection in detections:
-        bbox = [detection.Left, detection.Top, detection.Width, detection.Height]
-	    raw_dets.append((bbox, detection.Confidence, detection.ClassID))
-        print(detection)
+		bbox = [detection.Left, detection.Top, detection.Width, detection.Height]
+		class_name = label_arr[detection.ClassID]
+		raw_dets.append((bbox, detection.Confidence, class_name))
+		print(detection)
 
-    tracks = tracker.update_tracks(np_source, raw_dets)
-    
-    show_frame = np_source.copy()
-    drawer.draw_tracks(
-                show_frame,
-                tracks,
-                chosen_track=chosen_track
-            )
-    show_frame = cv2.cvtColor(show_frame, cv2.COLOR_BGR2RGBA)
-    display_img = jetson.utils.cudaFromNumpy(show_frame)
+	tracks = tracker.update_tracks(np_source, raw_dets)
+
+	show_frame = np_source.copy()
+	if raw_dets: 	
+		drawer.draw_tracks(
+			show_frame,
+			tracks,
+			chosen_track=chosen_track
+		)
+	#show_frame = cv2.cvtColor(show_frame, cv2.COLOR_BGR2RGBA)
+	#display_img = jetson.utils.cudaFromNumpy(show_frame)
+	cv2.imshow("webcam", show_frame)
+	k = cv2.waitKey(30)
+	if k == ord('q'):
+		break
 	# render the image
-	output.Render(display_img))
+	#output.Render(display_img)
 
 	# update the title bar
-	output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
+	#output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
 
 	# print out performance info
 	net.PrintProfilerTimes()
